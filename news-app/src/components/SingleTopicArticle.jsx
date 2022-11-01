1d439f094c7b8e8f4b28c669e035edd8f1e3eaa4
Kamakazee <cam_va@yahoo.co.uk>
2022-11-01 16:09:06 +0000

    Optimistic updating of likes

--- news-app/src/components/SingleTopicArticle.jsx
+++ news-app/src/components/SingleTopicArticle.jsx
@@ -3,15 +3,29 @@ import * as API from '../API.js'
 
 const SingleArticleByTopicItem = ({article, img_url})=>{
 
-    const [voteText, setVoteText] = useState("Like")
+    const [likeText, setLikeText] = useState("Like")
+    const [likes, setLikes] = useState(article.votes)
 
 
     const handleClick = (event)=>{
 
-        setVoteText((currentText)=>{
+        setLikeText((currentText)=>{
             if (currentText==="Like"){
+
+                setLikes((currentLikes)=>{
+                    return currentLikes + 1
+                })
+
+
+
                 return "Unlike"
             } 
+
+            setLikes((currentLikes)=>{
+                return currentLikes - 1
+            })
+
+            
 
             return "Like"
         })
@@ -19,18 +33,15 @@ const SingleArticleByTopicItem = ({article, img_url})=>{
     }
 
 
-
-
-
     return <div className="article-item">
     <p className="topicarticle">Title: {article.title}</p>
     <img className="topicarticle-profileimg" src={img_url}/>
     <p className="topicarticle">By: {article.author}</p>
     <p className="topicarticle">Comments: {article.comment_count}</p>
-    <p className="topicarticle">Likes: {article.votes}</p>
+    <p className="topicarticle">Likes: {likes}</p>
     <button onClick={()=>{
         handleClick()
-    }}>{voteText}</button>
+    }}>{likeText}</button>
 
     </div>
 

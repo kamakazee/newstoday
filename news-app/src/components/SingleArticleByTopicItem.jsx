e36800855e476ca6aeacefb85a1c4a00ac56b687
Kamakazee <cam_va@yahoo.co.uk>
2022-11-01 12:20:45 +0000

    Individual of topic articles with basic styling

---
+++ news-app/src/components/SingleArticleByTopicItem.jsx
@@ -0,0 +1,14 @@
+const SingleArticleByTopicItem = ({article})=>{
+
+
+    return <div className="article-item">
+    <p className="article-label">Article title</p>
+    <p className="article-label">{article.title}</p>
+    <p className="article-label">Article author</p>
+    <p className="article-label">{article.author}</p>
+
+    </div>
+
+}
+
+export default SingleArticleByTopicItem
--- news-app/src/components/SingleArticlesByTopic.jsx
+++ news-app/src/components/SingleArticlesByTopic.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import * as API from '../API.js'
+import SingleArticleByTopicItem from "./SingleArticleByTopicItem.jsx"
 
 
 const SingleArticlesByTopic = ()=>{
 
     const {topic} = useParams()
 
+    const [articles, setArticles] = useState([])
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(()=>{
+
+        setLoading(true)
+
+        API.fetchArticlesByTopic(topic).then((articles)=>{
+            console.log(articles)
+            setArticles(articles)
+            setLoading(false)
+        })
+
+    }, [])
+
+
+    if (isLoading) return <p>Loading....</p>
+
     return( <>
     <h2>Inside of single page of Articles By Topic: {topic}</h2>
+    {articles.map((article, index)=>{
+        return <SingleArticleByTopicItem key={index} article={article}/>
+    })}
     </>
     )
 }

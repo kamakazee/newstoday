3c1e3c428bdf8fb76a431fadaf1886d779684485
Kamakazee <cam_va@yahoo.co.uk>
2022-11-01 10:49:26 +0000

    Elements within header stay within border on resizing of window

--- news-app/src/components/Header.jsx
+++ news-app/src/components/Header.jsx
@@ -4,14 +4,17 @@ import {Link} from 'react-router-dom'
 const Header = ({user})=>{
 
 
-    return <div className="header">
+    return (
+    <div className="header">
     
-    <div className="headerprofile-container">
-        <HeaderProfile user={user}/>
+        <div className="headerprofile-container">
+            <HeaderProfile user={user}/>
+        </div>
+        <div className="title-box">
+            <Link to="/" ><p className="title">newsToday</p></Link>
+        </div>
     </div>
-    <Link to="/" ><p className="title">newsToday</p></Link>
-
-    </div>
+    )
 
 }
 
